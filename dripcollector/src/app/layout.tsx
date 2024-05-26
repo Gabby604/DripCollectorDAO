e671cbf927960c5d159bb96682da5398cfee0477
Nadeem <nadeemareiqat@gmail.com>
2024-05-26 16:59:57 +0300

    DripCollector

--- dripcollector/src/app/layout.tsx
+++ dripcollector/src/app/layout.tsx
@@ -7,8 +7,30 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Drip Collector",
-  description: "",
+  description: "An innovative tool for collecting and managing your drips.",
+  keywords: "drip collector, manage drips, productivity tool",
+  openGraph: {
+    type: 'website',
+    title: 'Drip Collector',
+    description: 'An innovative tool for collecting and managing your drips.',
+    images: [
+      {
+        url: '../../public/LinkImage.png',
+        width: 800,
+        height: 600,
+        alt: 'Drip Collector',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@yourTwitterHandle',
+    title: 'Drip Collector',
+    description: 'An innovative tool for collecting and managing your drips.',
+  },
+  viewport: 'width=device-width, initial-scale=1.0',
 };
+
 
 export default function RootLayout({
   children,
